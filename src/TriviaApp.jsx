460116dc61521f3c3b06cad2b4b8cc2305dec357
DaffaPermadi <daffagt123@gmail.com>
2025-04-30 13:16:06 +0700

    View Oke

--- src/TriviaApp.jsx
+++ src/TriviaApp.jsx
@@ -2,58 +2,59 @@ import React, { useEffect, useState } from "react";
 import FormQuestion from "./components/FormQuestion";
 import FormAnswer from "./components/FormAnswer";
 
-const quiz = [
-  {
-    id: 1,
-    question: "Siapa nama nabi ke 25?",
-    answer: "Muhammad",
-  },
-  {
-    id: 2,
-    question: "Dimana ibukota jepang?",
-    answer: "Tokyo",
-  },
-  {
-    id: 3,
-    question: "Batch kita berapa?",
-    answer: "326",
-  },
-  {
-    id: 4,
-    question: "Siapa nama panggilan direktur IT BNI?",
-    answer: "Pak Toto",
-  },
-  {
-    id: 5,
-    question: "Jam berapa istirahat makan siang (dalam angka)?",
-    answer: "12",
-  },
-];
+
 
 const TriviaApp = () => {
+
+  const quiz = [
+    {
+      id: 1,
+      question: "Siapa nama nabi ke 25?",
+      answer: "Muhammad",
+    },
+    {
+      id: 2,
+      question: "Dimana ibukota jepang?",
+      answer: "Tokyo",
+    },
+    {
+      id: 3,
+      question: "Batch kita berapa?",
+      answer: "326",
+    },
+    {
+      id: 4,
+      question: "Siapa nama panggilan direktur IT BNI?",
+      answer: "Pak Toto",
+    },
+    {
+      id: 5,
+      question: "Jam berapa istirahat makan siang (dalam angka)?",
+      answer: "12",
+    },
+  ];
+
   const [question, setQuestion] = useState(quiz[0]);
   const [number, setNumber] = useState(1);
-  const [answer, setAnswer] = useState([]);
+  const [answer, setAnswer] = useState("");
 
-  useEffect(() => {});
-
-  //   function handleChangeQuestion() {
-  //     console.log("Number ", number);
-  //     setNumber(number + 1);
-  //     if (number > 5) {
-  //       console.log(number);
-  //       console.log("Ini di handle");
-  //     }
-  //     setQuestion(quiz[number - 1]);
-  //     console.log(question);
-  //   }
+  function handleChangeQuestion() {
+    console.log("Number ", number);
+    if (number >= quiz.length) {
+      alert("Quiz Selesai");
+      return;
+    }
+    setNumber(number + 1);
+    setQuestion(quiz[number - 1]);
+    console.log(question);
+  }
 
   return (
     <div className="flex flex-col justify-center items-center h-screen ">
       <FormQuestion question={question} number={number} />
       <FormAnswer
-        setAnswer={handleChangeQuestion}
         value={answer}
+        setAnswer={setAnswer}
         onChange={(e) => setAnswer(e.target.value)}
         changeQuestion={handleChangeQuestion}
       />
--- src/components/FormAnswer.jsx
+++ src/components/FormAnswer.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const FormAnswer = ({ setAnswer, value, submit, changeQuestion }) => {
   return (
-    <form className="w-full max-w-sm" onSubmit={submit}>
+    <>
+    {/* // <form className="w-full max-w-sm" onSubmit={submit}> */}
       <div className="flex items-center border-b border-teal-500 py-2">
         <input
           className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
-          placeholder="Jane Doe"
-          aria-label="Full name"
           value={value}
-          onChange={setAnswer((e) => e.target.value)}
+          onChange={(e)=>setAnswer(e.target.value)}
         />
         <button
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
@@ -20,7 +19,8 @@ const FormAnswer = ({ setAnswer, value, submit, changeQuestion }) => {
           Next
         </button>
       </div>
-    </form>
+    {/* </form> */}
+    </>
   );
 };
 
